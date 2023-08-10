a342ab0b5289ea6082f692fae92db80764454d23
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-08-10 19:12:38 +0300

    Test CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 export default function App() {
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <h2 className="text-lg">
+                Just testing the architecture
+            </h2>
+        </>
+    )
 }
