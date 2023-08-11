45f9dcb39f497ff4104c1fae2541822602b4361d
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-08-11 22:15:48 +0300

    fix skills grid

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,9 +13,9 @@ export default function Skills() {
                 <p className="skill col-span-4">ELK stack</p>
                 <p className="skill col-span-3">K8s</p>
                 <p className="skill col-span-5">etc.</p>
-                <p className="skill-link col-span-5 flex flex-row justify-center items-center space-x-6">
-                    <i className="bi bi-linkedin text-3xl"/>
-                    <p className="align-middle">more on LinkedIn</p>
+                <p className="skill-link col-span-6 flex flex-row justify-center items-center">
+                    <i className="bi bi-linkedin text-3xl text-center w-1/4"/>
+                    <p className="text-base text-center w-3/4">more on LinkedIn</p>
                 </p>
             </div>
         </div>
