051a0778e04e5aba168b8d2c0b5cb798d728cd2e
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-08-12 01:02:58 +0300

    add acting phrase

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,7 +1,8 @@
 export default function Contacts() {
     return (
         <div className="flex flex-col">
-            <p className="heading">Contacts</p>
+            <p className="heading-contacts">Contacts</p>
+            <p className="mb-4 lg:mb-8">Feel free to contact with me ✍🏻</p>
             <div className="flex flex-col space-y-4 lg:flex-row lg:space-y-0 lg:space-x-20">
                 <div className="flex flex-row justify-center items-center rounded-2xl w-full border border-stone-50 p-2">
                     <i className="bi bi-linkedin text-3xl text-center w-1/5"/>
